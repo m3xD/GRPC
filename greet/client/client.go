aa8c844dabf3b90a148aa7ee0a3322de83ea9e37
duykhanh <thaoproduction123@gmail.com>
2024-03-20 11:52:59 +0700

    finish handling deadline

--- greet/client/client.go
+++ greet/client/client.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"context"
+	pb "grpc/Greet/proto"
+	"log"
+	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+)
+
+func getName(client pb.GreetServerClient, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+
+	defer cancel()
+
+	res, err := client.Greet(ctx, &pb.GetRequest{
+		FirstName: "Khanh",
+	})
+
+	if err != nil {
+		_, ok := status.FromError(err)
+
+		if ok {
+			log.Fatalln("An exxceed deadline")
+			return
+		} else {
+			log.Fatalln("Not an grpc error")
+		}
+	}
+
+	log.Println("The result is", res.Result)
+}
+
+func main() {
+	c, err := grpc.Dial("localhost:50069", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalln("Error", err)
+	}
+
+	defer c.Close()
+
+	client := pb.NewGreetServerClient(c)
+
+	getName(client, 3*time.Second)
 }
--- greet/server/server.go
+++ greet/server/server.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	pb "grpc/greet/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+type server struct {
+	pb.GreetServerServer
+}
+
+func (*server) Greet(ctx context.Context, in *pb.GetRequest) (*pb.GetResponse, error) {
+	for i := 0; i < 3; i++ {
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Print("The client canceled the request")
+			return nil, status.Error(
+				codes.DeadlineExceeded,
+				"The client canceled the request",
+			)
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	return &pb.GetResponse{Result: "Hello " + in.GetFirstName()}, nil
+}
+
 func main() {
-	addr := "localhost:50000"
+	addr := "localhost:50069"
 	lis, err := net.Listen("tcp", addr)
 
 	if err != nil {
@@ -19,6 +43,8 @@ func main() {
 	fmt.Printf("Listening on %s\n", addr)
 
 	s := grpc.NewServer()
+
+	pb.RegisterGreetServerServer(s, &server{})
 
 	err = s.Serve(lis)
 
